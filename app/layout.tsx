8419a8a5529f3015279220b388f37fc4228dd526
Mehmet-Durna <mehmetdurna0011@gmail.com>
2023-11-19 16:22:56 +0100

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,21 @@
-import type { Metadata } from 'next'
-import {Navbar,Footer} from "@/components/index";
-import './globals.css'
+import "./globals.css";
+import {Footer, NavBar} from "@/components";
 
 
-export const metadata: Metadata = {
-  title: 'Car Showcase',
-  description: 'Discover the best cars in the entire city, with the best prices and the best brands.',
-}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover world's best car showcase application",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className="relative">
-      <Navbar/>
-      {children}
-      <Footer/>
+    <html lang='en'>
+      <body className='relative'>
+        <NavBar />
+        {children}
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,49 +1,46 @@
 "use client";
-import Image from 'next/image'
+
+import Image from "next/image";
 import {CustomButton} from "@/components/index";
 
 
-function Hero() {
 
-    const handleScroll = () => {
-        const nextSection = document.getElementById("discover");
+const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
-        if (nextSection) {
-            nextSection.scrollIntoView({ behavior: "smooth" });
-        }
-    };
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-    return (
-        <div className="hero">
-            <div className="flex-1 pt-36 padding-x">
-                <h1 className="hero__title">
-                    Find. book or rent a car quickly and easily!
-                </h1>
-                <p className="hero__description">
-                    We have the best cars in the entire city, with the best prices and the best brands.
-                </p>
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, rent a car—quick and super easy!
+        </h1>
 
-                <CustomButton
-                    title="Explore Cars"
-                    containerStyles="bg-primary-blue text-white rounded-full mt-10"
-                    handleClick={handleScroll}
-                />
-            </div>
-            <div className="hero__image-container">
-                <div className="hero__image">
-                    <Image
-                        src="/hero.png"
-                        alt="hero"
-                        fill
-                        className="object-contain"
-                    />
-                    <div className="hero__image-overlay"/>
-                </div>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
 
-            </div>
-
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
-    );
-}
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
